ab86fc077a8ba38e3b44aa7ebe0ab75d6d07e4ea
Adam Ameen <adamameen5@gmai.com>
2021-01-11 02:58:01 +0530

    Added the contents in the homepage

---
+++ Readify/Readify/Models/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  Readify
+//
+//  Created by Adam Ameen on 2021-01-10.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgItem: UIImageView!
+    
+}
