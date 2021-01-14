8b67b4fb1ffff58aa3c02e1d31aa1200aeb79f24
Adam Ameen <adamameen5@gmai.com>
2021-01-14 17:25:04 +0530

    Updated the segmented control UI

--- Readify/Readify/Views/BooksViewController.swift
+++ Readify/Readify/Views/BooksViewController.swift
@@ -11,100 +11,115 @@
 
 import UIKit
 
+struct cellData {
+    let bookTitle : String!
+    let bookAuthor : String!
+}
+
+
 class BooksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //Variables
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
     @IBOutlet weak var tableViewControl: UITableView!
+    var arrayOfCellDataCompleted = [cellData]()
+    var arrayOfCellDataReading = [cellData]()
+    var arrayOfCellWishlist = [cellData]()
     
-//    let reading = ["Harry Potter", "Game of Thrones"]
-//    let completed = ["Harry Potter 1"]
-//    let wishlist = ["Narnia"]
-    
-    var data = [
-            ["J.K.Rowling HarryPotter", "Stalen GameofThrones"],
-            ["J.K.Rowling HarryPotter1", "Stalen GameofThrones2"],
-            ["EDWIN HarryPotter", "ALFRED GameofThrones"]
-        ]
-    
-    var p: Int!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tableViewControl.delegate = self
-//        tableViewControl.dataSource = self
-        let nib = UINib(nibName: "CustomTableViewCell", bundle: nil)
-        tableViewControl.register(nib, forCellReuseIdentifier: "customCell")
         
-        p=0;
-//         Do any additional setup after loading the view.
+        tableViewControl.delegate = self
+        tableViewControl.dataSource = self
+        
+        arrayOfCellDataCompleted = [cellData(bookTitle: "Harry", bookAuthor: "HarryAuthor"),
+                                    cellData(bookTitle: "Jone", bookAuthor: "JonesAuthor"),
+                                    cellData(bookTitle: "Mark", bookAuthor: "MarksAuthor")]
+        
+        arrayOfCellDataReading = [cellData(bookTitle: "RdBook1", bookAuthor: "RdBook1Author"),
+                                  cellData(bookTitle: "RdBook2", bookAuthor: "RdBook2Author"),
+                                  cellData(bookTitle: "RdBook3", bookAuthor: "RdBook3Author"),
+                                  cellData(bookTitle: "RdBook4", bookAuthor: "RdBook4Author")]
+        
+        arrayOfCellWishlist = [cellData(bookTitle: "WSBook1", bookAuthor: "WsBook1Author"),
+                               cellData(bookTitle: "WSBook2", bookAuthor: "WsBook2Author"),
+                               cellData(bookTitle: "WSBook3", bookAuthor: "WsBook3Author"),
+                               cellData(bookTitle: "WSBook4", bookAuthor: "WsBook4Author"),
+                               cellData(bookTitle: "WSBook5", bookAuthor: "WsBook5Author"),
+                               cellData(bookTitle: "WSBook6", bookAuthor: "WsBook6Author")]
+
     }
     
+    
+    // Creates the relevant number of rows in the view based on the number of records in the array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch segmentedControl.selectedSegmentIndex {
-//                case 0:
-//                    return reading.count
-//                case 1:
-//                    return completed.count
-//                case 2:
-//                    return wishlist.count
-//                default:
-//                    break
-//                }
-//                return 0
-        
-        return data[p].count
+        switch segmentedControl.selectedSegmentIndex {
+                case 0:
+                    return arrayOfCellDataReading.count
+                case 1:
+                    return arrayOfCellDataCompleted.count
+                case 2:
+                    return arrayOfCellWishlist.count
+                default:
+                    break
+                }
+                return 0
         
     }
+    
+    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//
-//         switch segmentedControl.selectedSegmentIndex {
-//         case 0:
-//             cell.textLabel?.text = reading[indexPath.row]
-//         case 1:
-//             cell.textLabel?.text = completed[indexPath.row]
-//         case 2:
-//             cell.textLabel?.text = wishlist[indexPath.row]
-//         default:
-//             break
-//         }
-//         return cell
-        
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
-        let str = data[p][indexPath.row].components(separatedBy: " ")
-        cell.customInit(text: str[1], author: str[0])
     
-        cell.contentView.layer.cornerRadius = 10
-        cell.contentView.frame = cell.contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
-        return cell
+    
+    //Loads the relevant data to the relevant segment control using the XIB file
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        
+        if (segmentedControl.selectedSegmentIndex == 0){
+            
+            let cellForReading = Bundle.main.loadNibNamed("CustomTableViewCell", owner: self, options: nil)?.first as! CustomTableViewCell
+            cellForReading.BookTitle.text = arrayOfCellDataReading[indexPath.row].bookTitle
+            cellForReading.Author.text = arrayOfCellDataReading[indexPath.row].bookAuthor
+            
+            //styling
+            cellForReading.contentView.layer.cornerRadius = 10
+            cellForReading.contentView.frame = cellForReading.contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+            
+            return cellForReading
+            
+        } else if (segmentedControl.selectedSegmentIndex == 1) {
+            
+            let cellForCompleted = Bundle.main.loadNibNamed("CustomCompletedTableViewCell", owner: self, options: nil)?.first as! CustomCompletedTableViewCell
+            cellForCompleted.bookTitle.text = arrayOfCellDataCompleted[indexPath.row].bookAuthor
+            cellForCompleted.bookAuthor.text = arrayOfCellDataCompleted[indexPath.row].bookAuthor
+            
+            //styling
+            cellForCompleted.contentView.layer.cornerRadius = 10
+            cellForCompleted.contentView.frame = cellForCompleted.contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+            
+            return cellForCompleted
+            
+        } else {
+            
+            let cellForWishlist = Bundle.main.loadNibNamed("CustomWishListTableViewCell", owner: self, options: nil)?.first as! CustomWishListTableViewCell
+            cellForWishlist.bookTitle.text = arrayOfCellWishlist[indexPath.row].bookTitle
+            cellForWishlist.bookAuthor.text = arrayOfCellWishlist[indexPath.row].bookAuthor
+            
+            //styling
+            cellForWishlist.contentView.layer.cornerRadius = 10
+            cellForWishlist.contentView.frame = cellForWishlist.contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+            return cellForWishlist
+        }
         
     }
     
 
+    
     @IBAction func segmentedChanges(_ sender: UISegmentedControl) {
-        p = sender.selectedSegmentIndex
         tableViewControl.reloadData()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
